ee4bdbc167dbaac34e6552b46e58d7b39cca1bf3
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-27 13:02:42 +0200

    first update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function init() {
+  
+    new Vue({
+        
+      el: '#app',
+      
+      data: {
+
+      },
+      
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
