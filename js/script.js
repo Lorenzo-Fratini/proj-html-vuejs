cd57028917ce465534ea6962df35150188a3b28e
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-28 12:43:59 +0200

    header update

--- js/script.js
+++ js/script.js
@@ -6,7 +6,100 @@ function init() {
       
       data: {
 
+        logo: 'logo-2x.png',
+
+        dropHover: false,
+
+        navItems: [
+          {
+            value: 'Home',
+            dropdown: false,
+            dropdownShow: '',
+            arrowDownActive: 'active',
+            arrowTopActive: '',
+            button: false,
+          },
+          {
+            value: 'Apple',
+            dropdown: false,
+            dropdownShow: '',
+            arrowDownActive: '',
+            arrowTopActive: '',
+            button: false,
+          },
+          {
+            value: 'Microsoft',
+            dropdown: false,
+            dropdownShow: '',
+            arrowDownActive: '',
+            arrowTopActive: '',
+            button: false,
+          },
+          {
+            value: 'Android',
+            dropdown: false,
+            dropdownShow: '',
+            arrowDownActive: '',
+            arrowTopActive: '',
+            button: false,
+          },
+          {
+            value: 'Forums',
+            dropdown: [
+              'Apple',
+              'Android',
+              'Microsoft',
+              'Apps & Software',
+              'Gadget & Stuff',
+              'General Discussion',
+              'News & Community',
+              'Tutorials & Guide'
+            ],
+            dropdownShow: '',
+            arrowActive: '',
+            arrowTopActive: '',
+            button: false
+          },
+          {
+            value: 'Contact us',
+            dropdown: false,
+            dropdownShow: '',
+            arrowDownActive: '',
+            arrowTopActive: '',
+            button: false,
+          },
+          {
+            value: 'Join Us',
+            dropdown: ['Log in', 'Lost your password?'],
+            dropdownShow: '',
+            arrowDownActive: '',
+            arrowTopActive: '',
+            button: true
+          }
+        ]
       },
+
+      methods: {
+
+        navHover: function(index) {
+
+          this.dropHover = !this.dropHover;
+
+          this.navItems[index].dropdownShow = this.dropHover ? 'show' : '';
+          this.navItems[index].arrowDownActive = this.dropHover ? 'active' : '';
+          this.navItems[index].arrowTopActive = this.dropHover
+          && this.navItems[index].dropdown != '' ? 'active' : '';
+
+          this.navItems[0].arrowDownActive = 'active';
+
+          console.log(this.navItems[index].arrowTopActive);
+        }
+      },
+
+      mounted() {
+
+        console.log('Here');
+      }
       
     })
   }
