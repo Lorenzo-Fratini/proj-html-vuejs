df5b031f916daf1c48fac1b77c19f054ff280e85
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-29 13:04:30 +0200

    animation commit

--- js/script.js
+++ js/script.js
@@ -1,196 +1,193 @@
 function init() {
   
-    new Vue({
-        
-      el: '#app',
+  new Vue({
       
-      data: {
+    el: '#app',
+    
+    data: {
 
-        logo: 'logo-2x.png',
+      logo: 'logo-2x.png',
 
-        dropHover: false,
+      dropHover: false,
 
-        inputActive: '',
+      inputActive: '',
 
-        layoverActive: '',
+      layoverActive: '',
 
-        navItems: [
-          {
-            value: 'Home',
-            dropdown: false,
-            dropdownShow: '',
-            arrowDownActive: 'active',
-            arrowTopActive: '',
-            button: false,
-          },
-          {
-            value: 'Apple',
-            dropdown: false,
-            dropdownShow: '',
-            arrowDownActive: '',
-            arrowTopActive: '',
-            button: false,
-          },
-          {
-            value: 'Microsoft',
-            dropdown: false,
-            dropdownShow: '',
-            arrowDownActive: '',
-            arrowTopActive: '',
-            button: false,
-          },
-          {
-            value: 'Android',
-            dropdown: false,
-            dropdownShow: '',
-            arrowDownActive: '',
-            arrowTopActive: '',
-            button: false,
-          },
-          {
-            value: 'Forums',
-            dropdown: [
-              'Apple',
-              'Android',
-              'Microsoft',
-              'Apps & Software',
-              'Gadget & Stuff',
-              'General Discussion',
-              'News & Community',
-              'Tutorials & Guide'
-            ],
-            dropdownShow: '',
-            arrowActive: '',
-            arrowTopActive: '',
-            button: false
-          },
-          {
-            value: 'Contact us',
-            dropdown: false,
-            dropdownShow: '',
-            arrowDownActive: '',
-            arrowTopActive: '',
-            button: false,
-          },
-          {
-            value: 'Join Us',
-            dropdown: ['Log in', 'Lost your password?'],
-            dropdownShow: '',
-            arrowDownActive: '',
-            arrowTopActive: '',
-            button: true
-          }
-        ],
-
-        footerLists: [
-          {
-            title: 'Popular Topics',
-            listItems: [
-              {
-                value: 'Nam a dolor volutpat massa auctor semper'
-              },
-              {
-                value: 'Sed euismod nunc urna fermentum arcu dapibus fringilla.'
-              },
-              {
-                value: 'Cras lacinia tellus id mauris finibus lacus molestie'
-              },
-              {
-                value: 'Curabitur a scelerisque ligula'
-              }
-            ]
-          },
-          {
-            title: 'Recent Topics',
-            listItems: [
-              {
-                value: 'Cras lacinia tellus id mauris finibus lacus molestie'
-              },
-              {
-                value: 'Proin at ligula sagittis vestibulum nisi vitae'
-              },
-              {
-                value: 'Sed nec metus at est tincidunt elementum'
-              },
-              {
-                value: 'Nulla egestas nulla eu nulla suscipit molestie'
-              }
-            ]
-          },
-          {
-            title: 'Latest Replies',
-            listItems: [
-              {
-                value: 'Cras lacinia tellus id mauris finibus lacus molestie'
-              },
-              {
-                value: 'Cras lacinia tellus id mauris finibus lacus molestie'
-              },
-              {
-                value: 'Nulla egestas nulla eu nulla suscipit molestie'
-              },
-              {
-                value: 'Mauris ac nibh quis eros sagittis lacinia a et dui'
-              }
-            ]
-          }
-        ],
-
-        footerIcons: [
-          {
-            value: 'fa-facebook-f',
-            class: 'bg-facebook'
-          },
-          {
-            value: 'fa-twitter',
-            class: 'bg-twitter'
-          },
-          {
-            value: 'fa-instagram',
-            class: 'bg-instagram'
-          },
-          {
-            value: 'fa-youtube',
-            class: 'bg-youtube'
-          }
-        ],
-      },
-
-      methods: {
-
-        navHover: function(index) {
-
-          this.dropHover = !this.dropHover;
-
-          this.navItems[index].dropdownShow = this.dropHover ? 'show' : '';
-          this.navItems[index].arrowDownActive = this.dropHover ? 'active' : '';
-          this.navItems[index].arrowTopActive = this.dropHover
-          && this.navItems[index].dropdown != '' ? 'active' : '';
-
-          this.navItems[0].arrowDownActive = 'active';
-
-          console.log(this.navItems[index].arrowTopActive);
+      navItems: [
+        {
+          value: 'Home',
+          dropdown: false,
+          dropdownShow: '',
+          arrowDownActive: 'active',
+          arrowTopActive: '',
+          button: false,
         },
-
-        searchShow: function() {
-  
-          this.inputActive = 'active';
-          this.layoverActive = 'active';
+        {
+          value: 'Apple',
+          dropdown: false,
+          dropdownShow: '',
+          arrowDownActive: '',
+          arrowTopActive: '',
+          button: false,
         },
-
-        searchHide: function() {
-          this.inputActive = '';
-          this.layoverActive = '';
+        {
+          value: 'Microsoft',
+          dropdown: false,
+          dropdownShow: '',
+          arrowDownActive: '',
+          arrowTopActive: '',
+          button: false,
+        },
+        {
+          value: 'Android',
+          dropdown: false,
+          dropdownShow: '',
+          arrowDownActive: '',
+          arrowTopActive: '',
+          button: false,
+        },
+        {
+          value: 'Forums',
+          dropdown: [
+            'Apple',
+            'Android',
+            'Microsoft',
+            'Apps & Software',
+            'Gadget & Stuff',
+            'General Discussion',
+            'News & Community',
+            'Tutorials & Guide'
+          ],
+          dropdownShow: '',
+          arrowActive: '',
+          arrowTopActive: '',
+          button: false
+        },
+        {
+          value: 'Contact us',
+          dropdown: false,
+          dropdownShow: '',
+          arrowDownActive: '',
+          arrowTopActive: '',
+          button: false,
+        },
+        {
+          value: 'Join Us',
+          dropdown: ['Log in', 'Lost your password?'],
+          dropdownShow: '',
+          arrowDownActive: '',
+          arrowTopActive: '',
+          button: true
         }
+      ],
+
+      footerLists: [
+        {
+          title: 'Popular Topics',
+          listItems: [
+            {
+              value: 'Nam a dolor volutpat massa auctor semper'
+            },
+            {
+              value: 'Sed euismod nunc urna fermentum arcu dapibus fringilla.'
+            },
+            {
+              value: 'Cras lacinia tellus id mauris finibus lacus molestie'
+            },
+            {
+              value: 'Curabitur a scelerisque ligula'
+            }
+          ]
+        },
+        {
+          title: 'Recent Topics',
+          listItems: [
+            {
+              value: 'Cras lacinia tellus id mauris finibus lacus molestie'
+            },
+            {
+              value: 'Proin at ligula sagittis vestibulum nisi vitae'
+            },
+            {
+              value: 'Sed nec metus at est tincidunt elementum'
+            },
+            {
+              value: 'Nulla egestas nulla eu nulla suscipit molestie'
+            }
+          ]
+        },
+        {
+          title: 'Latest Replies',
+          listItems: [
+            {
+              value: 'Cras lacinia tellus id mauris finibus lacus molestie'
+            },
+            {
+              value: 'Cras lacinia tellus id mauris finibus lacus molestie'
+            },
+            {
+              value: 'Nulla egestas nulla eu nulla suscipit molestie'
+            },
+            {
+              value: 'Mauris ac nibh quis eros sagittis lacinia a et dui'
+            }
+          ]
+        }
+      ],
+
+      footerIcons: [
+        {
+          name: 'Facebook',
+          icon: 'fa-facebook-f',
+          class: 'bg-facebook'
+        },
+        {
+          name: 'Twitter',
+          icon: 'fa-twitter',
+          class: 'bg-twitter'
+        },
+        {
+          name: 'Instagram',
+          icon: 'fa-instagram',
+          class: 'bg-instagram'
+        },
+        {
+          name: 'YouTube',
+          icon: 'fa-youtube',
+          class: 'bg-youtube'
+        }
+      ],
+    },
+
+    methods: {
+
+      navHover: function(index) {
+
+        this.dropHover = !this.dropHover;
+
+        this.navItems[index].dropdownShow = this.dropHover ? 'show' : '';
+        this.navItems[index].arrowDownActive = this.dropHover ? 'active' : '';
+        this.navItems[index].arrowTopActive = this.dropHover
+        && this.navItems[index].dropdown != '' ? 'active' : '';
+
+        this.navItems[0].arrowDownActive = 'active';
       },
 
+      searchShow: function() {
 
-      mounted() {
+        this.inputActive = 'active';
+        this.layoverActive = 'active';
+      },
 
-        console.log('Here');
+      searchHide: function() {
+        
+        this.inputActive = '';
+        this.layoverActive = '';
       }
-      
-    })
-  }
-  
-  document.addEventListener('DOMContentLoaded', init);
+    },
+    
+  })
+}
+
+document.addEventListener('DOMContentLoaded', init);
